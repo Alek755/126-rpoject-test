64241833874b5279dfb77197f25411a0e2d107ad
Alek755 <80863873+Alek755@users.noreply.github.com>
2023-03-17 12:44:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-song1 = "";
-song2 = "";
-
+song = "";
 leftWristX = 0;
 leftWristY = 0;
-
 rightWristX = 0;
 rightWristY = 0;
 
 function preload() {
-    song1 = loadSound("fmttm.mp3");
-    song2 = loadSound("wh.mp3");
+    song = loadSound("music.mp3");
+    
 }
 
 function setup() {
-    canvas = createCanvas(650, 495);
-    canvas.center()
+    canvas = createCanvas(650, 500);
+    canvas.center();
     video = createCapture(VIDEO);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on("pose", gotPoses);
+
+}
+
+function modelLoaded() {
+    console.log("MODEL IS LOADED");
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+
+        LeftWristScore = results[0].pose.keypoints[9].score;
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -33,14 +37,29 @@ function gotPoses(results) {
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("RIGHT WRIST X = " + rightWristX + " | RIGHT WRIST Y = " + rightWristY);
-
     }
-}
 
-function modelLoaded() {
-    console.log("MODEL IS LOADED");
 }
 
 function draw() {
-    image(video, 0, 0, 650, 495);
+    image(video, 0, 0, 650, 500);
+
+    fill("red");
+    stroke("red");
+
+    if(LeftWristScore > 0.2) {
+
+    circle(leftWristX, leftWristY, 10);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("disc_volume").innerHTML = "VOLUME = " + volume;
+    song.setVolume(volume);
+    }
+
+}
+
+function play() {
+    song.play();
+    song.rate(1);
 }
